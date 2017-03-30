82c6f6189713a2301cc625887b72b5606707eaef
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2017-03-29 23:05:59 -0700

    added credentials to email.js

--- config/email.js
+++ config/email.js
@@ -1,8 +1,8 @@
 module.exports.email = {
   service: 'Mailgun',
   auth: {
-    user: process.env.user,
-    pass: process.env.pass
+    user: 'postmaster@sandboxed6580cad9444d1b9679c5d0e4416d74.mailgun.org',
+    pass: 'e52adbda25d0526b0935ff8eb2b15ac3'
   },
   templateDir: 'api/emailTemplates',
   from: 'MyPortfolio',
